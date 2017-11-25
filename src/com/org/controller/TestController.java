1cf5d615378cad26a5578cced92433a85fbe4669
Prashant Kadam <kadam.prashant1993@gmail.com>
2017-11-25 21:19:58 +0530

    changes1

--- src/com/org/controller/TestController.java
+++ src/com/org/controller/TestController.java
@@ -1,5 +1,21 @@
 package com.org.controller;
 
-public class TestController {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServlet;
+
+public class TestController extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException ,java.io.IOException {
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
+		dispatcher.forward(req, resp);
+		
+		
+	}
 
 }
