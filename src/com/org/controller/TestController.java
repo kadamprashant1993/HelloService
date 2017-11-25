8e723cfc19835234138740312d497e6cd5d15f9d
Prashant Kadam <kadam.prashant1993@gmail.com>
2017-11-25 21:26:56 +0530

    test3

--- src/com/org/controller/TestController.java
+++ src/com/org/controller/TestController.java
@@ -10,8 +10,8 @@ public class TestController extends HttpServlet{
 	 */
 	private static final long serialVersionUID = 1L;
 	@Override
-	protected void doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException ,java.io.IOException {
-		
+	public void doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp) throws javax.servlet.ServletException ,java.io.IOException {
+		System.out.println("Test3");
 		RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
 		dispatcher.forward(req, resp);
 		
